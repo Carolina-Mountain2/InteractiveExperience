d0dea8090a8fcbdbfd1e6c5150ff82a48f8ed172
Brendan Mulhern <bmmacomb@sbcglobal.net>
2015-03-29 14:16:31 -0400

    Question parser added
    Added a question parser, a questions xml, and the 'mainclass' file which
    will be our main game class. all questions MUST be typed in the same
    style that i used in the xml.
    The xml reader doesn't recognize spaces so i used a minus sign instead
    and removed/replaced it later otherwise this should work for us when we
    get questions. the examples include a multi choice and a true or false
    question.

---
+++ Assets/MainScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainScript : MonoBehaviour {
+	public TextAsset xml;
+	QuestionParser QP;
+	public static MainScript S;
+	public List<Question> Qs;
+	public Question[] questions;
+
+
+
+	// Use this for initialization
+	void Start () {
+		S = this;
+
+		QP = GetComponent<QuestionParser> ();  
+		Qs = new List<Question>();
+
+
+
+		parseQuestions ();
+		questions = Qs.ToArray();
+
+		print (questions [1].correct);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void parseQuestions(){
+		//print (xml.text);
+		QP.read (xml.text);
+
+	}
+}
