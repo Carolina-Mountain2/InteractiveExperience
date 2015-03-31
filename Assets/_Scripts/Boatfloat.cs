6289583a8a0492603af37bf82c74580d46a85b9b
Andrew Madden <amadden@unca.edu>
2015-03-31 15:30:58 -0400

    Cleaned up some extra files. Slightly altered terrain.

---
+++ Assets/_Scripts/Boatfloat.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boatfloat : MonoBehaviour {
+
+		public float UpwardForce = 12.0f; // 9.81 is the opposite of the default gravity, which is 9.81. If we want the boat not to behave like a submarine the upward force has to be higher than the gravity in order to push the boat to the surface
+		private bool isInWater = false;
+		
+		void OnTriggerEnter(Collider collidier) {
+			isInWater = true;
+			rigidbody.drag = 5f;
+		}
+		
+		void OnTriggerExit(Collider collidier) {
+			isInWater = false;
+			rigidbody.drag = 0.05f;
+		}
+		
+		void FixedUpdate() {
+			if(isInWater) {
+				// apply upward force
+				Vector3 force = transform.up * UpwardForce;
+				this.rigidbody.AddRelativeForce(force, ForceMode.Acceleration);
+				Debug.Log("Upward force: " + force+" @"+Time.time);
+			}
+		}
+	}
+
---
+++ Assets/_Scripts/Question.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Question {
+	public string qText;
+	public enum qType{
+		truefalse,
+		multiChoice
+	}
+	public qType type;
+	public string[] answers;
+	public string QID;
+	public string correct;
+	public int corChoice;
+
+	// Use this for initialization
+	public void create(string id, string qtext, string type ,string[] qanswers,int cchoice)
+	{
+		this.qText = qtext;
+		if (type == "truefalse") {
+			this.type = qType.truefalse;
+			this.answers = new string[2];
+			answers[0] = "true";
+			answers[1] = "false";
+			this.QID = id;
+			this.corChoice = cchoice;
+
+
+		}
+		else if (type == "multichoice"){
+			this.type = qType.multiChoice;
+			this.answers = new string[qanswers.Length];
+			this.answers = qanswers;
+			this.QID = id;
+			this.corChoice = cchoice;
+		}
+	
+
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
