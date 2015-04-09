688423543a5f549ff2d0f323d87b363c7d15a1f4
tyds83 <tyds83g@gmail.com>
2015-04-09 19:22:01 -0400

    Refined menu to show question example
    Placed button to unpause have gui box to show questions

--- Assets/_Scripts/Pause.cs
+++ Assets/_Scripts/Pause.cs
@@ -1,41 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pause : MonoBehaviour {
+public class Pause : MonoBehaviour
+{
 
-	public bool paused = false;
-	public static Pause S;
+		public bool paused = false;
+		public static Pause S;
 
-	void Start(){
-		S = this;
-		togglePause (false);
-	}
-	void Update() {
+
+
+
+
+		void Start ()
+		{
+				S = this;
+				togglePause (false);
 
 		}
 
-	void OnGui () {
-	
-		if (paused == true) {
-			
+		void Update ()
+		{
 
-			print("game paused");
-			
-		} 
-		
-	}
-
-
-
-	
-	public void togglePause(bool isPaused)
-	{
-		
-		if (isPaused == true) {
-			paused = true;
-			Time.timeScale = 0;
-
-			print("game paused");
 		}
-	}
+
+		public Texture tex;
+
+		void OnGUI ()
+		{
+
+				if (paused) {
+			GUILayout.Box(MainScript.S.questions[0].qText);
+		
+			if (GUI.Button (new Rect (280, 200, 100, 50), "unpause me!")) {
+								print ("game unpaused");
+								togglePause(false);
+
+										}
+				}
+		}
+
+		public void togglePause (bool isPaused)
+		{
+
+				if (isPaused == true) {
+						paused = true;
+						Time.timeScale = 0f;
+						print ("game paused");
+
+				} else {
+						paused = false;
+						Time.timeScale = 1f; 
+						print ("game paused");
+				}
+
+		}
 }
--- Assets/_Scripts/Plane.cs
+++ Assets/_Scripts/Plane.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class Plane : MonoBehaviour {
 
 	public bool paused = false;
+	public GameObject            qText;
 	// Use this for initialization
 	void Start () {
-	
+		//qText = GameObject.Find("questionText"); <-- in case GUI Box is inadequate
+
 	}
 	
 	// Update is called once per frame
@@ -17,9 +19,11 @@ public class Plane : MonoBehaviour {
 
 		if (col.tag == "Boat") {
 			print ("Boat hit plane");
+			//qText.guiText.text = MainScript.S.questions[0].qText; <-- in case GUI Box is inadequate
 			print (MainScript.S.questions[0].qText);
 			bool tem = true;
 			Pause.S.togglePause(tem);
+			Destroy(this);//destroy this plane's collider
 		}
 	}
 
