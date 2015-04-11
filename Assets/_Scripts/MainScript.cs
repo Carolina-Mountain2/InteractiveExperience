c676f6ac1d133c73df4b344ae068b000f912aaff
tyds83 <tyds83g@gmail.com>
2015-04-10 23:59:07 -0400

    Adds music, scoring system, damage system, et al
    I added a loop I made in GarageBand (can be turned off or removed). Put
    the scoring system in place. The damage system takes damage EVERY
    instance of “I Give Up” button pressed. Removed the Main Script.cs from
    _Scene_0 since it was throwing a null reference error due to the
    scoring system.

--- Assets/_Scripts/MainScript.cs
+++ Assets/_Scripts/MainScript.cs
@@ -8,17 +8,24 @@ public class MainScript : MonoBehaviour {
 	public static MainScript S; // the singleton for this class
 	public List<Question> Qs; // HOUSEKEEPING ONLY: a linked list containing the questions
 	public Question[] questions; // the array of questions: use this to retrieve questions
+	private static int score;
+	private static int hits;
+	public GameObject scoreText;
+	public GameObject hitText;
 
-
+	void Awake () {
+		scoreText = GameObject.Find("questionText");
+		hitText = GameObject.Find ("damageText");
+		}
 
 	// Use this for initialization
 	void Start () {
 		S = this;
-
 		QP = GetComponent<QuestionParser> ();  // the question parser, it should be attached to the same game object as this script
 		Qs = new List<Question>();// HOUSEKEEPING ONLY: a linked list containing the questions
 
-
+		scoreText.guiText.text = "Score: " + score;
+		hitText.guiText.text = "Hits left: " + hits;
 
 		parseQuestions ();
 		questions = Qs.ToArray(); // QUESTION ARRAY CREATED HERE; ANYTHING DEALING WITH QUESTIONS SHOULD BE BELOW THIS   
@@ -33,7 +40,10 @@ public class MainScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		hits = Pause.S.damage;
+		score = Pause.S.questPoint;
+		scoreText.guiText.text = "Score: " + score;
+		hitText.guiText.text = "Hits left: " + hits;
 	}
 	public void parseQuestions(){
 		//print (xml.text);
--- Assets/_Scripts/Pause.cs
+++ Assets/_Scripts/Pause.cs
@@ -7,49 +7,102 @@ public class Pause : MonoBehaviour
 		public static Pause S;
 		MouseLook MouseLookScriptX;
 		MouseLook MouseLookScriptY;
+		string feedback;
 		int ranQuest = 0;
+		public int questPoint = 0;
+		public int damage = 3;
 
-
-		void Start () {
+		void Start ()
+		{
 				S = this;
 				togglePause (false);
-				GameObject x = GameObject.Find("First Person Controller");
-				GameObject y = GameObject.Find("Main Camera");
-				MouseLookScriptX = x.GetComponent<MouseLook>();
-				MouseLookScriptY = y.GetComponent<MouseLook>();
+				GameObject x = GameObject.Find ("First Person Controller");
+				GameObject y = GameObject.Find ("Main Camera");
+				MouseLookScriptX = x.GetComponent<MouseLook> ();
+				MouseLookScriptY = y.GetComponent<MouseLook> ();
+		}
+
+		void Update ()
+		{
 
 		}
 
-		void Update () {
-
-		}
-
-		void OnGUI () {
+		void OnGUI ()
+		{
 			GUI.skin.box.wordWrap = true;
-				if (paused) {
 
-						GUI.Box (new Rect (280, 80, 500, 200), MainScript.S.questions [ranQuest].qText);
-								if (GUI.Button (new Rect (280, 300, 100, 50), "unpause me!")) {       
-								print ("game unpaused");
+				if (paused) {	
+					GUI.Box (new Rect (285, 80, 500, 200), MainScript.S.questions [ranQuest].qText + feedback);
+					if (GUI.Button (new Rect (165, 150, 100, 50), "I give up")) {//currently representing correct choice
+						questPoint = questPoint - 100;
+						damage--;
+						print (questPoint);
+						togglePause (false);
+						MouseLookScriptX.enabled = !MouseLookScriptX.enabled;
+						MouseLookScriptY.enabled = !MouseLookScriptY.enabled;
+					}
+	
+						/*
+								 * The new Rect for the below Buttons, when script is added
+								 * to make multiple choice, 280 is left most, add 100 to that
+								 * number to account width, then add 20 to create a nice space
+								 * between the buttons.
+								 * 
+								 * There needs to be an if-else that checks which type. When
+								 * that is done all you'll need to is copy the below script
+								 * which represents what a multichoice GUI looks like and add
+								 * it to the if multichoice statement.
+								 */
+						//button controllers
+						if (GUI.Button (new Rect (305, 300, 100, 50), "Correct Answer!")) {//currently representing correct choice
+								questPoint = questPoint + 100;
+								feedback = "";
+								print (questPoint);
 								togglePause (false);
 								MouseLookScriptX.enabled = !MouseLookScriptX.enabled;
 								MouseLookScriptY.enabled = !MouseLookScriptY.enabled;
+						}
+						if (GUI.Button (new Rect (425, 300, 100, 50), "incorrect!")) {//currently representing wrong choice
+								questPoint = questPoint - 25;
+								feedback = "\n Try Again";
+								print ("incorrect");
+								print (questPoint);
+								//togglePause (false);
 
 						}
+						if (GUI.Button (new Rect (545, 300, 100, 50), "incorrect!")) {//currently representing wrong choice
+								questPoint = questPoint - 25;
+								feedback = "\n Try Again";
+								print ("incorrect");
+								print (questPoint);
+								//togglePause (false);
+				
+						}
+						if (GUI.Button (new Rect (665, 300, 100, 50), "incorrect!")) {//currently representing wrong choice
+								questPoint = questPoint - 25;
+								feedback = "\n Try Again";
+								print ("incorrect");
+								print (questPoint);
+								//togglePause (false);
+				
+						}
+
+
 				}
-	}
+		}
 
 		public void togglePause (bool isPaused)
 		{
 
 				if (isPaused == true) {
-			ranQuest = Random.Range (2, 23);
+						ranQuest = Random.Range (2, 23);
 
 						paused = true;
 						Time.timeScale = 0f;
 						print ("game paused");
 						MouseLookScriptX.enabled = !MouseLookScriptX.enabled;
 						MouseLookScriptY.enabled = !MouseLookScriptY.enabled;
+						
 
 			           
 
